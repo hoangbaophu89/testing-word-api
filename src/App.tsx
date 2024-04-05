323c4efc4393e6bdafba9c9f7ed8651cfa1a6dcc
Hieu Vu <hieuvd@pcugroup.com>
2024-04-05 16:32:56 +0700

    ai test add

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import ReactDataSheet from 'react-datasheet';
 import './styles.css';
 import { Paper } from '@mui/material';
-import { ActionButton, ChoiceGroup, IChoiceGroupOption, ILabelStyles, IStackTokens, IStyleSet, Label, Pivot, PivotItem, PrimaryButton, Spinner, SpinnerSize, Stack } from '@fluentui/react';
+import { ActionButton, ChoiceGroup, IChoiceGroupOption, ILabelStyles, IStackTokens, IStyleSet, Label, Pivot, PivotItem, PrimaryButton, Spinner, SpinnerSize, Stack, TextField } from '@fluentui/react';
 import SelectEditor from './Select';
 import { initializeIcons } from '@fluentui/font-icons-mdl2';
 import { LatViewer, LongViewer } from './FillViewer';
@@ -225,7 +225,8 @@ const sampleData = {
 
 enum FileType {
   word = "word",
-  pdf = "pdf"
+  pdf = "pdf",
+  excel = "excel",
 }
 
 
@@ -239,14 +240,43 @@ function App() {
   const refInput = React.useRef() as React.MutableRefObject<HTMLInputElement>;
 
   const refEditor = React.useRef() as React.MutableRefObject<HTMLInputElement>;
+  const refEditor2 = React.useRef() as React.MutableRefObject<HTMLInputElement>;
 
   const [currentFile, setCurrentFile] = React.useState<any>();
 
   const [fileType, setFileType] = React.useState<FileType>(FileType.pdf);
   const [loading, setLoading] = React.useState<boolean>(false);
+  const [anaRes, setAnaRes] = React.useState<any>({});
 
   const dataref = React.useRef<any>(JSON.stringify(sampleData));
-
+  const fileRef = React.useRef<any>();
+  const prompt = React.useRef<string>(`You are an assistant designed to extract entities from an Image. 
+  Users will upload an image and you will respond with entities you've extracted from the image as a image as an Json object .Begin your response with {. 
+  The Output of Json format have some the following key:
+  - InvoiceNumber
+  - Supplier
+  - Address
+  - TransactionDate
+  - Category
+  - SubCategory
+  - Unit
+  - Amount
+  - Payment
+  - ReportAmount
+  - Tax
+  
+  InvoiceNumber: return string
+  Supplier: return string
+  Address: return string
+  TransactionDate: return ios string
+  Category: I define below. In an invoice, only take 1 single classification
+  *CategoryList
+  SubCategory: Corresponding to each Category, we have array of SubCategories, return the corresponding Json of SubCategory in array of SubCategories
+  Unit: Currency Unit, return string, code according to ISO 4217
+  Amount: return string
+  Payment: Payment Method, return string, one of the following 3 values: Cash, Credit, Company Credit
+  ReportAmount: Based on Amount, Unit and current currency exchange rates to calculate ReportAmount in *ReportUnit unit
+  Tax: return string`);
 
   React.useLayoutEffect(
     () => {
@@ -264,6 +294,19 @@ function App() {
     }, []
   );
 
+  React.useLayoutEffect(
+    () => {
+      const options = {
+        mode: 'code',
+      };
+      const jsoneditor = new JSONEditor(refEditor2.current, options as any);
+      jsoneditor.set(anaRes);
+      return () => {
+        jsoneditor.destroy();
+      }
+    }, [anaRes]
+  );
+
   const onChangeHandler = (event: any) => {
     console.log("file change");
     const file = event.target.files[0];
@@ -273,59 +316,112 @@ function App() {
   };
   return (
     <div style={{ width: "100%", padding: "30px", boxSizing: "border-box" }}>
-      <h1>Testing API CU VN</h1>
-      <div>
-        <div className="jsoneditor-react-container" style={{ height: "500px" }} ref={refEditor} />
-      </div>
-      <div>
-        <a target={"_blank"}
-          href="https://pcugroup-my.sharepoint.com/:w:/p/phuhb/EQurv-ucv5FGvM6Yi9wsFwYByRDasOMpOZqdInnG0OxLrA?e=0njcNv">
-          Dowload word template
-        </a>
-      </div>
-      <ChoiceGroup label="Result file type" defaultSelectedKey={FileType.pdf} options={options} onChange={(e, value) => {
-        setFileType(value?.key as FileType | FileType.pdf);
-      }} />
-      <ActionButton
-        onClick={() => {
-          refInput.current.click();
-        }}
-        iconProps={{ iconName: 'OpenFile' }} >Update word file {currentFile ? `: ${currentFile.name}` : ""}</ActionButton>
-      <input type="file" style={{ display: "none" }}
-        name="fileForLab" accept='.docx' ref={refInput} onChange={(e) => onChangeHandler(e)} />
-      <div>
-        <PrimaryButton disabled={loading} text="Ok" onClick={() => {
-          if (currentFile != null) {
-            setLoading(true);
-            const formData = new FormData();
-            formData.append("FormFile", currentFile);
-            formData.append("FileName", currentFile.name);
-            formData.append("DataToReplace", dataref.current);
-            formData.append("FileType", fileType);
-            try {
-              const config = { responseType: "blob" };
-              const res = axios.post("https://apilimsde.controlunion.com/Word/proccessing", formData, config as any).then(
-                result => {
-                  const file2 = new Blob([result.data], { type: result.headers["content-type"] });
-                  saveAs(file2, fileType == FileType.word ? currentFile.name : currentFile.name.replace(".docx", ".pdf"));
+      <>
+        <h1>Testing API CU VN</h1>
+        <div>
+          <div className="jsoneditor-react-container" style={{ height: "500px" }} ref={refEditor} />
+        </div>
+        <div>
+          <a target={"_blank"}
+            href="https://pcugroup-my.sharepoint.com/:w:/p/phuhb/EQurv-ucv5FGvM6Yi9wsFwYByRDasOMpOZqdInnG0OxLrA?e=0njcNv">
+            Dowload word template
+          </a>
+        </div>
+        <ChoiceGroup label="Result file type" defaultSelectedKey={FileType.pdf} options={options} onChange={(e, value) => {
+          setFileType(value?.key as FileType | FileType.pdf);
+        }} />
+        <ActionButton
+          onClick={() => {
+            refInput.current.click();
+          }}
+          iconProps={{ iconName: 'OpenFile' }} >Update word file asdasdsa {currentFile ? `: ${currentFile.name}` : ""}</ActionButton>
+        <input type="file" style={{ display: "none" }}
+          name="fileForLab" accept='.xlsx' ref={refInput} onChange={(e) => onChangeHandler(e)} />
+        <div>
+          <PrimaryButton disabled={loading} text="Ok"
+            onClick={() => {
+              if (currentFile != null) {
+                setLoading(true);
+                const formData = new FormData();
+                formData.append("FormFile", currentFile);
+                formData.append("FileName", currentFile.name);
+                formData.append("DataToReplace", dataref.current);
+                formData.append("FileType", fileType);
+                try {
+                  const config = { responseType: "blob" };
+                  const res = axios.post("http://localhost:7279/api/DocumentProcessing", formData, config as any).then(
+                    result => {
+                      const file2 = new Blob([result.data], { type: result.headers["content-type"] });
+                      saveAs(file2, fileType == FileType.pdf ? currentFile.name.replace(".docx", ".pdf") : currentFile.name);
+                      setLoading(false);
+                    }
+                  );
+                  console.log(res);
+                } catch (ex) {
                   setLoading(false);
+                  console.log(ex);
                 }
-              );
-              console.log(res);
-            } catch (ex) {
-              setLoading(false);
-              console.log(ex);
-            }
+              }
+              else {
+                alert("Pls upload file ");
+              }
+            }} allowDisabledFocus />
+          {
+            loading &&
+            <Spinner size={SpinnerSize.xSmall} />
           }
-          else {
-            alert("Pls upload file ");
+        </div>
+      </>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+        <h1>Testing Image Vision</h1>
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: 20 }}>
+          <TextField
+            label='Prompt'
+            multiline
+            rows={20}
+            defaultValue={prompt.current}
+            onChange={(e, value) => {
+              prompt.current = value || ""
+            }}
+          ></TextField>
+
+
+          <div >
+            <Label>Result</Label>
+            <div style={{ height: '350px' }} className="jsoneditor-react-container" ref={refEditor2} />
+          </div>
+        </div>
+
+        <input type="file"
+          onChange={(e: any) => {
+            const [file] = e.target.files;
+            fileRef.current = file;
+          }}
+        ></input>
+        <div style={{ display: 'flex' }}>
+          <PrimaryButton style={{ width: '100px' }} disabled={loading}
+            onClick={() => {
+              setLoading(true);
+              const formData = new FormData();
+              formData.append('promptString', prompt.current);
+              formData.append('file', fileRef.current, 'filename.jpg');
+              axios.post('https://cuai.azurewebsites.net/api/GPT4Vision?code=2Q8qqTp2bluQcJuz-cPm97ztbEOARps_J4sGhv5ZxfGWAzFuGW-eGA==', formData)
+                .then(resultAnlayze => {
+                  setAnaRes(resultAnlayze)
+                  setLoading(false);
+                })
+            }} allowDisabledFocus >
+            Analyse
+          </PrimaryButton>
+          {
+            loading &&
+            <Spinner size={SpinnerSize.xSmall} />
           }
-        }} allowDisabledFocus />
-        {
-          loading &&
-          <Spinner size={SpinnerSize.xSmall} />
-        }
+        </div>
+
       </div>
+
+
     </div>
   );
 }
